6aa04c3b3220e81a0db49b28876abb656c157a75
Krystian Nowakowski <krysti.8642@gmail.com>
2022-05-03 22:31:59 +0200

    added some code idk

---
+++ clock.js
@@ -0,0 +1,21 @@
+function countdown() {
+    let timestamp = 1653998400000;
+    let target = new Date(timestamp);
+    let now = new Date();
+    let time_left = Math.round((target - now) / 1000);
+
+    let days = Math.floor(time_left / 86400);
+    time_left %= 86400;
+    let hours = Math.floor(time_left / 3600);
+    time_left %= 3600;
+    let minutes = Math.floor(time_left / 60);
+    let seconds = time_left % 60;
+    let out = "";
+
+    out += `${days}<br>${String(hours).padStart(2, "0")}:`;
+    out += `${String(minutes).padStart(2, "0")}:`;
+    out += `${String(seconds).padStart(2, "0")}`;
+
+    document.getElementById("clock").innerHTML = out;
+    setTimeout(countdown, 1000);
+}
